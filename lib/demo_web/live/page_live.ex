cb9449430843463fb467c167fc3be04ff4132ec9
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-04-21 19:41:05 -0500

    Add basic example of counter

--- lib/demo_web/live/page_live.ex
+++ lib/demo_web/live/page_live.ex
@@ -5,6 +5,29 @@ defmodule DemoWeb.PageLive do
   use DemoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, number: 0)}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div class="w-full text-center">
+      <strong>
+        <%= @number %>
+        Hello World or not?
+      </strong>
+      <.button class="bg-blue-600 hover:bg-blue-300" phx-click="add">Add</.button>
+      <.test />
+    </div>
+    """
+  end
+
+  def test(assigns) do
+    ~H"""
+      This is a text or maybe not?
+    """
+  end
+
+  def handle_event("add", _params, socket) do
+    {:noreply, assign(socket, number: socket.assigns.number+1)}
   end
 end
