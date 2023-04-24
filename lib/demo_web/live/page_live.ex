c3822f4f349ce8f3b638264c456bd98d1ac6ba00
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-04-23 23:59:15 -0500

    Add live example

--- lib/demo_web/live/page_live.ex
+++ lib/demo_web/live/page_live.ex
@@ -5,29 +5,30 @@ defmodule DemoWeb.PageLive do
   use DemoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, number: 0)}
-  end
-
-  def render(assigns) do
-    ~H"""
-    <div class="w-full text-center">
-      <strong>
-        <%= @number %>
-        Hello World or not?
-      </strong>
-      <.button class="bg-blue-600 hover:bg-blue-300" phx-click="add">Add</.button>
-      <.test />
-    </div>
-    """
+    {:ok, assign(socket, number: 0, form: to_form(%{adding_amount_default: 8}))}
   end
 
   def test(assigns) do
     ~H"""
-      This is a text or maybe not?
+    This is a text or maybe not?
     """
   end
 
   def handle_event("add", _params, socket) do
-    {:noreply, assign(socket, number: socket.assigns.number+1)}
+    {:noreply, assign(socket, number: socket.assigns.number + 1)}
+  end
+
+  def handle_event("adding_more", %{"add_amount" => the_added_amount}, socket) do
+    amount_to_add_by =
+      case Integer.parse(the_added_amount) do
+        {number, _} -> number
+        :error -> 0
+      end
+
+    {:noreply,
+     assign(socket,
+       number: socket.assigns.number + amount_to_add_by,
+       form: to_form(%{adding_amount_default: amount_to_add_by})
+     )}
   end
 end
